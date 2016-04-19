231419c8f80f594e4c07a76312e6ab8aa7195786
Tong <tzhou21@asu.edu>
2016-04-18 20:55:42 -0700

    Remove GetConsoleWindow
    No need of using GetConsoleWindow, change to broadcast instead

--- TurnOffLCD/Program.cs
+++ TurnOffLCD/Program.cs
@@ -9,11 +9,9 @@ namespace TurnOffLCD
 {
     class Program
     {
-        [DllImport("kernel32.dll")]
-        private static extern IntPtr GetConsoleWindow();
 
         [DllImport("user32.dll")]
-        private static extern int SendMessage(IntPtr hWnd, int hMsg, int Wparam, int lParam);
+        private static extern int SendMessage(int hWnd, int hMsg, int Wparam, int lParam);
         private const int SC_MONITORPOWER = 0xF170;
         private const int WM_SYSCOMMAND = 0x0112;
         private const int HWND_BROADCAST = 0xFFFF;
@@ -23,7 +21,7 @@ namespace TurnOffLCD
         private const int MONITOR_STANDBY = 1;
         static void Main(string[] args)
         {
-            SendMessage(GetConsoleWindow(), WM_SYSCOMMAND, SC_MONITORPOWER, MONITOR_OFF);
+            SendMessage(HWND_BROADCAST, WM_SYSCOMMAND, SC_MONITORPOWER, MONITOR_OFF);
         }
     }
 }
